34eac6d16cbbb646ac47f9d1b9b7a658db8bdf1d
Max <maxim.nebela@gmail.com>
2021-02-25 21:50:27 +0200

    feat: add full screen

--- react-game/src/App.tsx
+++ react-game/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense, useEffect} from "react";
+import React, {Suspense, useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {
     answer, backToMainMenu, refreshBoard,
@@ -14,10 +14,11 @@ import {
 import {Route, Switch, useHistory} from "react-router-dom"
 import {Game} from "./components/Game/Game";
 import useSound from "use-sound";
-import {GithubOutlined} from "@ant-design/icons/lib";
+import {FullscreenExitOutlined, FullscreenOutlined, GithubOutlined} from "@ant-design/icons/lib";
 //@ts-ignore
 import music from "./sounds/music.mp3"
 import hotkeys from "hotkeys-js";
+import { Button } from "antd";
 
 
 export type CardType = {
@@ -38,6 +39,7 @@ const App:React.FC<{}> = React.memo(() => {
     const boardSize = useSelector(getBoardSize)
     const history = useHistory()
     const autoPlay = useSelector(getAutoPlay)
+    const [isFullScreen, setIsFullScreen] = useState(false)
     const [play, {stop}] = useSound(music, {volume: Number(localStorage.getItem("musicVolume") || musicVolume) , interrupt: true})
     useEffect(()=>{
         gameType==="wait"?play():stop()
@@ -81,10 +83,47 @@ const App:React.FC<{}> = React.memo(() => {
 
     const boardItemsCount = boardSize === "small" ? 4 : boardSize === "normal" ? 8 : boardSize === "big" ? 10 : 18
     const boardItems: Array<CardType> = Array(boardItemsCount).fill(0).map((e:number,index:number) => ({content:`${index}`, type:"closed", id:index}))
+    const game = document.getElementById("game")
+    const handleClick = () => {
+        if(game){
+            if (game.requestFullscreen) {game.requestFullscreen()
+                setIsFullScreen(true)}
+            //@ts-ignore
+            else if (game.webkitRequestFullscreen) {game.webkitRequestFullscreen()
+                setIsFullScreen(true)}
+            //@ts-ignore
+            else if (game.msRequestFullscreen) {game.msRequestFullscreen()
+                setIsFullScreen(true)}
+        }
+    } // https://www.w3schools.com/howto/howto_js_fullscreen.asp
+    const handleOff = () => {
+        const docWithBrowsersExitFunctions = document as Document & {
+            mozCancelFullScreen(): Promise<void>;
+            webkitExitFullscreen(): Promise<void>;
+            msExitFullscreen(): Promise<void>;
+        };
+        if (docWithBrowsersExitFunctions.exitFullscreen) {
+            setIsFullScreen(false)
+            docWithBrowsersExitFunctions.exitFullscreen();
+        } else if (docWithBrowsersExitFunctions.webkitExitFullscreen) {
+            setIsFullScreen(false)
+            docWithBrowsersExitFunctions.webkitExitFullscreen();
+        } else if (docWithBrowsersExitFunctions.msExitFullscreen) {
+            setIsFullScreen(false)
+            docWithBrowsersExitFunctions.msExitFullscreen();
+        }
+    } // https://www.w3schools.com/howto/howto_js_fullscreen.asp
 
     return (
         <div className="App">
-            <div className={"game"}>
+
+            <div className={"game"} id={"game"}>
+                <div className={"fullScreen"}>
+                    {isFullScreen
+                        ? <Button size={"large"} onClick={handleOff}><FullscreenExitOutlined /></Button>
+                        : <Button size={"large"}  onClick={handleClick}><FullscreenOutlined /></Button>
+                    }
+                </div>
                 <Switch>
                     <Route  path={"/settings"} render={()=><Suspense fallback={<div>Loading</div>}><Settings /></Suspense>}/>
                     <Route  path={"/stats"} render={()=><Suspense fallback={<div>Loading</div>}><Stats /></Suspense>}/>
